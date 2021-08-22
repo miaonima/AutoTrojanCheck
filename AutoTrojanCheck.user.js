598fe4d326fda8626a860421c94f44826e66c02b
yhz <35647076+miaonima@users.noreply.github.com>
2021-08-21 19:05:26 -0700

    Added files and readme

---
+++ AutoTrojanCheck.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         AutoTrojanCheck
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://trojancheck.usc.edu/*
+// @icon         https://www.google.com/s2/favicons?domain=usc.edu
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var intervalId = window.setInterval( function() {
+        // Click Begin (Assessment) Next or Submit if any exists on page.
+        $(".btn-begin-assessment").click();
+        $(".btn-assessment-start").click();
+        $(".btn-next").click();
+        $(".btn-submit").click();
+
+        // Click No
+        $("div.mat-button-toggle-label-content:contains('No')").click();
+
+        // Check the checkboxes
+        let checkbox = $(".mat-checkbox-input");
+        if(checkbox && !checkbox.prop("checked")) {
+            checkbox.click();
+        }
+    }, 500);
+})();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AutoTrojanCheck
  An automatation script for completing daily TrojanCheck required by USC students.
+ See https://trojancheck.usc.edu/ for details.
